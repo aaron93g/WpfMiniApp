1d732b731a94a7487768d0a10508e065fb690744
aaron <aaron@ARHYP>
2023-06-07 00:20:24 -0600

    Add project files.

---
+++ WpfAppLibrary/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLibrary.Models
+{
+    public class AddressModel
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string FullAddress => $"{Street} {City}, {State} {ZipCode}";
+
+        public string Email { get; set; }
+    }
+}
---
+++ personInfoForm/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppLibrary.Models;
+
+namespace personInfoForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<PersonModel> people = new BindingList<PersonModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fullNameListBox.ItemsSource = people;
+            
+        }
+
+        private void saveNameButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameTextBox.Text,
+                LastName = lastNameTextBox.Text,
+            };
+
+            people.Add(person);
+        }
+    }
+}
